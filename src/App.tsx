2e62b7589ca6c1c044cd7605f026c4f36baad769
unknown <bryandang10@gmail.com>
2024-02-02 15:29:08 -0800

    comment out old

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,41 @@
 
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 
 function App() {
-  const [colour, setColour] = useState('');
+  // const [colour, setColour] = useState('');
 
-  const onclick = async () => {
-    let [tab] = await chrome.tabs.query({ active: true });
-    chrome.scripting.executeScript<string[], void>({
-      target: { tabId: tab.id! },
-      args: [colour],
-      func: (colour) => {
-        // alert('Hello from my extension!');
-        document.body.style.backgroundColor = colour;
-      }
-    });
+  // const onclick = async () => {
+  //   let [tab] = await chrome.tabs.query({ active: true });
+  //   chrome.scripting.executeScript<string[], void>({
+  //     target: { tabId: tab.id! },
+  //     args: [colour],
+  //     func: (colour) => {
+  //       // alert('Hello from my extension!');
+  //       document.body.style.backgroundColor = colour;
+  //     }
+  //   });
+  // }
+
+  const optionsClick = async () => {
+    console.log("clicked on options");
   }
 
   return (
     <>
       <h1>Duck Blocker</h1>
       <div className="card">
-        <input type='color' onChange={(e) => setColour(e.currentTarget.value)}/>
-        <button onClick={() => onclick()}>
-          click me
-        </button>
+        {/* <input type='color' onChange={(e) => setColour(e.currentTarget.value)}/> */}
 
         <Stack spacing={2} direction="column">
-          <Button variant="contained">Turn On</Button>
-          <Button variant="contained">Options</Button>
+          <Button variant="contained">
+            Turn On
+          </Button>
+          <Button variant="contained" onClick={() => optionsClick()}>
+            Options
+          </Button>
         </Stack>
       </div>
       <p>
