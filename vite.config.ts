d57a4ebe9347c5138b1e6f9a4383fe75a77c6f03
jerrydngzh <jerrydeng67@gmail.com>
2024-02-02 19:57:10 -0800

    get service workers running

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,17 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   build: {
+    outDir: 'dist',
     rollupOptions: {
       input: {
-        main: './index.html',
-        options: './src/pages/options.html'
+        main: 'index.html',
+        pages: './src/pages/options.html',
+        sw: './src/service-worker.js'
+      },
+      output: {
+        entryFileNames: '[name].js',
+        chunkFileNames: 'chunks/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash][extname]'
       }
     }
   }
